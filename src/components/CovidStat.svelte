eb0d6b847d676bf07d63a96cc847fc5f4ec7991d
daubejb <daubejb@gmail.com>
2020-10-09 12:26:53 -0400

    provide error handling 404 and 500

--- src/components/CovidStat.svelte
+++ src/components/CovidStat.svelte
@@ -1,5 +1,12 @@
 <script>
-  export let usStats;
+  export let cases;
+  export let deaths;
+  export let recovered;
+  export let ventilator;
+  export let hospitalized;
+  export let icu;
+  export let tested;
+  export let updated;
 </script>
 
 <style>
@@ -11,20 +18,20 @@
 <div class="section">
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Cases: {usStats.cases}</h2>
-      <h2 class="is-3 column">Deaths: {usStats.deaths}</h2>
-      <h2 class="is-3 column">Recovered: {usStats.recovered}</h2>
-      <h2 class="is-3 column">Updated: {usStats.updated}</h2>
+      <h2 class="is-3 column">Cases: {cases}</h2>
+      <h2 class="is-3 column">Deaths: {deaths}</h2>
+      <h2 class="is-3 column">Recovered: {recovered}</h2>
+      <h2 class="is-3 column">Updated: {updated}</h2>
     </div>
   </div>
 </div>
 <div class="section">
   <div class="container">
     <div class="columns">
-      <h2 class="is-3 column">Tested: {usStats.tested}</h2>
-      <h2 class="is-3 column">Hospitalized: {usStats.hospitalized}</h2>
-      <h2 class="is-3 column">Total ICU: {usStats.icu}</h2>
-      <h2 class="is-3 column">Total Ventilated: {usStats.ventilator}</h2>
+      <h2 class="is-3 column">Tested: {tested}</h2>
+      <h2 class="is-3 column">Hospitalized: {hospitalized}</h2>
+      <h2 class="is-3 column">Total ICU: {icu}</h2>
+      <h2 class="is-3 column">Total Ventilated: {ventilator}</h2>
     </div>
   </div>
 </div>
